3a857448867412c8a5082ba23f1950d701d42e5d
Luis Bree <LuisBree@gmail.com>
2025-07-22 02:36:26 +0000

    observo esto

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // This is to allow connections from the development environment (Firebase Studio)
-    allowedDevOrigins: ['https://*.cloudworkstations.dev'],
-  },
+  // This is to allow connections from the development environment (Firebase Studio)
+  allowedDomains: ['https://*.cloudworkstations.dev'],
   typescript: {
     ignoreBuildErrors: true,
   },
